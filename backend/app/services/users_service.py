1e64fbc6181d97f40448d145b9c2eb536b5fb520
thivaagar28 <thivaagar28@gmail.com>
2024-02-29 11:43:26 +0800

    Cleanup and solved change password id anomaly on update

--- backend/app/services/users_service.py
+++ backend/app/services/users_service.py
@@ -2,7 +2,7 @@ from typing import Optional
 from uuid import UUID
 import pymongo
 #import database models, schemes
-from app.schemas.users_schema import UserCreate, UserOut, UserUpdate
+from app.schemas.users_schema import UserCreate, UserUpdate, UserChangePassword
 from app.models.users_model import Users
 #import password hashing security function
 from app.core.security import get_hash_password, verify_password
@@ -50,7 +50,7 @@ class UsersService:
         return user
 
     @staticmethod
-    async def change_password(user_id: UUID, old_password: str, new_password: str):
+    async def change_password(user_id: UUID, old_password: str, new_password: str) -> Users:
         user = await UsersService.get_user_by_id(user_id)
         if not user:
             raise pymongo.errors.OperationFailure("User not found")
@@ -58,9 +58,11 @@ class UsersService:
         if not verify_password(password=old_password, hashed_pass=user.hashed_password):
             return None
         
-        user.hashed_password = get_hash_password(new_password)
+        user_pass_instance = UserChangePassword(
+            user_id= user.user_id,
+            hashed_password= get_hash_password(new_password)
+        )
 
-        await user.update({"$set": user.model_dump(exclude_unset=True)})
+        await user.update({"$set": user_pass_instance.model_dump(exclude_unset=True)})
         return user
-        #need tp check why the id is being created?
         
