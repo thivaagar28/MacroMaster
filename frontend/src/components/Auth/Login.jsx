5d895a2266f4aaa117d9d91393626c7d8e6bcbee
thivaagar28 <thivaagar28@gmail.com>
2024-03-06 19:09:50 +0800

    basic login page component code

---
+++ frontend/src/components/Auth/Login.jsx
@@ -0,0 +1,9 @@
+import { Flex, FormControl, Heading, useColorModeValue, Input, FormErrorMessage, Button, useToast } from "@chakra-ui/react"
+import { useForm } from "react-hook-form"
+import {UseNavigate, useNavigate} from "react-router-dom";
+
+export const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
