8c16215e893f78aa3c8e836433357413dc8fc7bf
thivaagar28 <thivaagar28@gmail.com>
2024-03-21 17:19:32 +0800

    added button click routing

--- frontend/src/components/NavBar/NavItem.jsx
+++ frontend/src/components/NavBar/NavItem.jsx
@@ -1,11 +1,12 @@
 import { Flex, Icon, Link, Menu, MenuButton, Text } from '@chakra-ui/react'
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-export const NavItem = ({icon, title, collapse, active, description}) => {
+export const NavItem = ({icon, title, collapse, path}) => {
+  const navigate = useNavigate();
   return (
     <Flex mt={30} flexDir={'column'} w={'100%'} alignItems={collapse ? 'flex-start' : 'center'}>
         <Menu >
-            <Link p={3} borderRadius={8} _hover={{textDecor: 'none', backgroundColor: 'RGBA(255, 255, 255, 0.24)'}} w={collapse && '100%'}>
+            <Link p={3} borderRadius={8} _hover={{textDecor: 'none', backgroundColor: 'RGBA(255, 255, 255, 0.24)'}} w={collapse && '100%'} onClick={() => navigate(`${path}`, {replace: true})}>
                 <MenuButton w={'100%'}>
                     <Flex>
                         <Icon as={icon} fontSize={'xl'}/>
