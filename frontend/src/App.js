618141928448118ed96a5492e0f8b4d7536f608d
thivaagar28 <thivaagar28@gmail.com>
2024-03-21 17:17:57 +0800

    Added AuthProvider Wrapper, spinner when not initialized, and wrapped routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,38 @@
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
-import { Login } from './components/Auth/Login';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import { DefaultLayout } from './components/views/layouts/DefaultLayout';
+import { Login } from './components/Auth/Login';
+import { Register } from './components/Auth/Register';
+import { PublicRoute } from './components/Auth/PublicRoute';
+import { AuthRoute } from './components/Auth/AuthRoute';
+import { AuthConsumer, AuthProvider} from './context/JWTAuthContext'
+import { Flex, Spinner } from '@chakra-ui/react';
+import { AccountSettings } from './components/User/AccountSettings';
 
 function App() {
   return (
-    < DefaultLayout>
-    <Router>
-      <Routes>
-        <Route path="/" element={<h1>Home content</h1>} />
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<h1>register</h1>}/>
-      </Routes>
-    </Router>
-    </DefaultLayout>
+    <AuthProvider>
+      <Router>
+        <AuthConsumer>
+            {(auth) => !auth.isInitialized ? ( /* It will auto run the jwt auth context script */
+              <DefaultLayout>
+                <Flex height={'100vh'} alignItems={'center'} justifyContent={'center'}>
+                <Spinner thickness='4px' speed='0.65s' emptyColor='green.200' color='green.500' size={'xl'}></Spinner>
+                </Flex>
+              </DefaultLayout>
+            ) : (
+              <DefaultLayout>
+                <Routes>
+                <Route path="/" element={<h1>Home content</h1>} />
+                <Route path='*' element={<Navigate to={'/'}/>} />
+                <Route path='/login' element={<PublicRoute><Login/></PublicRoute>}/>
+                <Route path='/register' element={<PublicRoute><Register/></PublicRoute>}/>
+                <Route path='/account_settings' element={<AuthRoute><AccountSettings/></AuthRoute>}/>
+              </Routes>
+              </DefaultLayout>
+            )}
+          </AuthConsumer>
+      </Router>
+    </AuthProvider>
   );
 }
 
