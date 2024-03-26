62a74d337f4031be567f00e8e1a554b4dbbb5ad6
thivaagar28 <thivaagar28@gmail.com>
2024-03-26 23:58:01 +0800

    Added Chakra tooltips theme modification

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme, cssVar} from '@chakra-ui/react';
+
+const $arrowBg = cssVar("popper-arrow-bg");
+const theme = extendTheme({
+  components: {
+    Tooltip: {
+      // Styles for the base style
+      baseStyle: {},
+      // Styles for the size variations
+      sizes: {},
+      // Styles for the visual style variations
+      variants: {
+        blackMode: () => {
+          return {
+            color: "white",
+            p: "2",
+            fontWeight : "md",
+            bg: "black",
+            borderRadius: "md",
+            [$arrowBg.variable]: "black"
+          };
+        }
+      },
+      // The default `size` or `variant` values
+      defaultProps: {}
+    }
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <App />
   </ChakraProvider>
 );
