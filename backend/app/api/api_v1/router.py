0d0e68e7cfdde57616c0cbb23dbab2f521638509
thivaagar28 <thivaagar28@gmail.com>
2024-05-20 19:54:21 +0800

    added usa router

--- backend/app/api/api_v1/router.py
+++ backend/app/api/api_v1/router.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 # import handlers for 
 from app.api.api_v1.handlers import users
 from app.api.auth.jwt import auth_router
+from app.api.api_v1.handlers import usa
 
 router = APIRouter()
 
 router.include_router(users.user_router, prefix='/users', tags=["Users"])
 router.include_router(auth_router, prefix='/auth', tags=["Authentication"])
+router.include_router(usa.usa_router, prefix='/usa', tags=["USA data"])
