b89b442d84e2d1e7539680bc1a567bc837139ed7
thivaagar28 <thivaagar28@gmail.com>
2024-05-24 03:38:09 +0800

    Added trend check function, fed data into necessary components and deconstructed macrodata.jsx

--- frontend/src/components/Home/MacroIndex.jsx
+++ frontend/src/components/Home/MacroIndex.jsx
@@ -1,25 +1,56 @@
-import { Divider, Flex, Text } from '@chakra-ui/react'
+import { Divider, Flex, Text, Spinner } from '@chakra-ui/react'
 import { IndexStat } from './IndexStat'
-import { MacroData } from './MacroData'
+import { SimpleGrid } from '@chakra-ui/react'
+import { MacroStat } from './MacroStat'
 
-export const MacroIndex = () => {
+export const MacroIndex = ({data, country}) => {
+  
+  const trend_check = (value) => {
+    return (value > 0)
+  }
+  
   return (
     <Flex mt={3} bg="RGBA(100,84,132, 0.51)" h={'40%'} borderRadius="2xl" position={'relative'} flexDir={'row'} justifyContent={'flex-start'}>
         <Flex bg={'#47406F'} w={'5%'} h={"full"}  borderTopLeftRadius={'2xl'} borderBottomLeftRadius={'2xl'} justifyContent={'center'} alignItems={'center'}>
             <Text fontSize={'lg'}  py={2} whiteSpace={'nowrap'} pos={'absolute'} h={'fit-content'} transformOrigin={'center'} transform={'rotate(-90.28deg)'}>Macroeconomic 101</Text>
         </Flex>
-        <Flex w={'full'} p={2} flexDir={'column'}>
+        {data ? 
+          <Flex w={'full'} p={2} flexDir={'column'}>
             <Flex h={'fit-content'} px={3} justifyContent={'space-around'}>
-                <IndexStat index_name='NASDAQ' ticker='^IXIC' desc='Nasdaq Composite'/>
-                <IndexStat index_name='S&P500' ticker='^GSPC'desc="Standard & Poor's 500 Index"/>
-                <IndexStat index_name='DJIA' ticker='^DJI' desc='Dow Jones Industrial Average'/>
-                <IndexStat index_name='CBOE VIX'ticker='^VIX' desc="Chicago Board Options Exchange's CBOE Volatility Index"/>
+              {country === 'USA' ?
+                <>
+                  <IndexStat index_name='NASDAQ' ticker='^IXIC' desc='Nasdaq Composite' value={data.ixic_close} perc={data.ixic}/>
+                  <IndexStat index_name='S&P500' ticker='^GSPC'desc="Standard & Poor's 500 Index" value={data.gspc_close} perc={data.gspc}/>
+                  <IndexStat index_name='DJIA' ticker='^DJI' desc='Dow Jones Industrial Average'value={data.dji_close} perc={data.dji}/>
+                  <IndexStat index_name='CBOE VIX'ticker='^VIX' desc="Chicago Board Options Exchange's CBOE Volatility Index" value={data.vix_close} perc={data.vix}/>
+                </>  
+               : <>
+                  <IndexStat index_name='KLCI'ticker='^KLSE' desc="FTSE Bursa Malaysia KLCI" value={data.klse_close}  perc={data.klse}/>
+               </>}
             </Flex>
             <Divider my={1}/>
             <Flex flex={1} px={5}>
-              <MacroData/>
+              <SimpleGrid w={'full'} columns={4} spacingX='5' spacingY='5px'>
+                <MacroStat name='Consumer Price Index (CPI)' perc={data.cpi} trend={trend_check(data.cpi)} />
+                <MacroStat name='Inflation Growth Rate' perc={data.inf} trend={trend_check(data.inf)} /> 
+                <MacroStat name='Goverment Reserves (Billions)' perc={data.ipi} trend={trend_check(data.cpi)} />
+                <MacroStat name='Industrial Production Index' perc={data.ipi} trend={trend_check(data.ipi)} />
+                <MacroStat name='Long Term Interest Rate' perc={data.lt_ir} trend={trend_check(data.lt_ir)} />
+                <MacroStat name='Short Term Interest Rate' perc={data.st_ir} trend={trend_check(data.st_ir)} />
+                <MacroStat name='Gross Domestic Product (GDP)' perc={data.gdpt} trend={trend_check(data.gdpt)} />
+                <MacroStat name='Unemployment Rate'  perc={data.ur} trend={trend_check(data.ur)}/>
+                <MacroStat name='Producer Price Index (PPI)'  perc={data.ppi} trend={trend_check(data.ppi)}/>
+                <MacroStat name='Wholesale & Retail Trade'  perc={data.wrt} trend={trend_check(data.wrt)}/>
+                <MacroStat name='Business Cycle Indicators (BCI)' perc={data.bcli} trend={trend_check(data.bcli)}/>
+                <Flex justifyContent={'center'} alignItems={'center'}>Data cut off Date: </Flex>
+              </SimpleGrid>
             </Flex>
-        </Flex>
+          </Flex> : 
+          <Flex w={'full'} h={'full'} justifyContent={'center'} alignItems={'center'}>
+            <Spinner thickness='4px' speed='0.65s' emptyColor='green.200' color='green.500' size={'xl'}/>
+          </Flex>
+        }
     </Flex>
+    
   )
 }
