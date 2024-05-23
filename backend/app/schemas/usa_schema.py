a93eec91cd53dba641e1464c15d23fb12c038247
thivaagar28 <thivaagar28@gmail.com>
2024-05-24 03:32:20 +0800

    removed statpoc6 and added standard stat, features, features for graph, cmpt and cmpt for graph response models

--- backend/app/schemas/usa_schema.py
+++ backend/app/schemas/usa_schema.py
@@ -1,7 +1,10 @@
-from typing import Optional
+from typing import Optional, List
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from beanie import Indexed
+
+class Month(BaseModel):
+    month: datetime
 
 class StatPoc1(BaseModel):
     bcli_poc1 : Optional[float]
@@ -24,27 +27,6 @@ class StatPoc1(BaseModel):
     vix_close : Optional[float]
     vix_poc1 : Optional[float]
 
-class StatPoc6(BaseModel):
-    bcli_poc6 : Optional[float]
-    cpi_poc6 : Optional[float]
-    gdpt_poc6 : Optional[float]
-    grb_poc6 : Optional[float]
-    inf_poc6 : Optional[float]
-    ipi_poc6 : Optional[float]
-    lt_ir_poc6 : Optional[float]
-    ppi_poc6 : Optional[float]
-    st_ir_poc6 : Optional[float]
-    ur_poc6 : Optional[float]
-    wrt_poc6 : Optional[float]
-    dji_close : Optional[float]
-    dji_poc6 : Optional[float]
-    gspc_close : Optional[float]
-    gspc_poc6 : Optional[float]
-    ixic_close : Optional[float]
-    ixic_poc6 : Optional[float]
-    vix_close : Optional[float]
-    vix_poc6 : Optional[float]
-
 class StatPoc12(BaseModel):
     bcli_poc12 : Optional[float]
     cpi_poc12 : Optional[float]
@@ -66,5 +48,76 @@ class StatPoc12(BaseModel):
     vix_close : Optional[float]
     vix_poc12 : Optional[float]
 
-class Month(BaseModel):
+class StandardStat(BaseModel):
+    bcli : Optional[float]
+    cpi : Optional[float]
+    gdpt : Optional[float]
+    grb : Optional[float]
+    inf : Optional[float]
+    ipi : Optional[float]
+    lt_ir : Optional[float]
+    ppi : Optional[float]
+    st_ir : Optional[float]
+    ur : Optional[float]
+    wrt : Optional[float]
+    dji_close : Optional[float]
+    dji : Optional[float]
+    gspc_close : Optional[float]
+    gspc : Optional[float]
+    ixic_close : Optional[float]
+    ixic : Optional[float]
+    vix_close : Optional[float]
+    vix : Optional[float]
+
+class FeaturesPoc1(BaseModel):
+    month : datetime
+    bcli_poc1 : Optional[float]
+    grb_poc1 : Optional[float]
+    inf_poc1 : Optional[float]
+    ipi_poc1 : Optional[float]
+    st_ir_poc1 : Optional[float]
+    gspc_poc1 : Optional[float]
+    ixic_poc1 : Optional[float]
+    vix_poc1 : Optional[float]
+
+class FeatureGraphPoc1(BaseModel):
+    month : List[datetime] = Field(default_factory=list)
+    bcli : List[Optional[float]] = Field(default_factory=list)
+    grb : List[Optional[float]] = Field(default_factory=list)
+    inf : List[Optional[float]] = Field(default_factory=list)
+    ipi : List[Optional[float]] = Field(default_factory=list)
+    st_ir : List[Optional[float]] = Field(default_factory=list)
+    gspc : List[Optional[float]] = Field(default_factory=list)
+    ixic : List[Optional[float]] = Field(default_factory=list)
+    vix : List[Optional[float]] = Field(default_factory=list)
+
+class FeaturesPoc12(BaseModel):
+    month : datetime
+    bcli_poc12 : Optional[float]
+    inf_poc12 : Optional[float]
+    lt_ir_poc12 : Optional[float]
+    ppi_poc12 : Optional[float]
+    gspc_poc12 : Optional[float]
+
+class FeatureGraphPoc12(BaseModel):
+    month : List[datetime] = Field(default_factory=list)
+    bcli : List[Optional[float]] = Field(default_factory=list)
+    inf : List[Optional[float]] = Field(default_factory=list)
+    lt_ir : List[Optional[float]] = Field(default_factory=list)
+    ppi : List[Optional[float]] = Field(default_factory=list)
+    gspc : List[Optional[float]] = Field(default_factory=list)
+
+class CmptPoc1(BaseModel):
     month: datetime
+    cmpt_poc1 : Optional[float]
+    pred_cmpt_poc1 : Optional[float]
+
+class CmptPoc12(BaseModel):
+    month: datetime
+    cmpt_poc12 : Optional[float]
+    pred_cmpt_poc12 : Optional[float]
+
+class CmptPoc(BaseModel):
+    month : List[datetime] = Field(default_factory=list)
+    cmpt : List[Optional[float]] = Field(default_factory=list)
+    pred_cmpt : List[Optional[float]] = Field(default_factory=list)
