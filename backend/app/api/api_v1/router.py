866ed5264a608eb527e7854119124e53a0a01b7b
thivaagar28 <thivaagar28@gmail.com>
2024-05-24 03:26:38 +0800

    Added malaysia router

--- backend/app/api/api_v1/router.py
+++ backend/app/api/api_v1/router.py
@@ -4,9 +4,11 @@ from fastapi import APIRouter
 from app.api.api_v1.handlers import users
 from app.api.auth.jwt import auth_router
 from app.api.api_v1.handlers import usa
+from app.api.api_v1.handlers import malaysia
 
 router = APIRouter()
 
 router.include_router(users.user_router, prefix='/users', tags=["Users"])
 router.include_router(auth_router, prefix='/auth', tags=["Authentication"])
 router.include_router(usa.usa_router, prefix='/usa', tags=["USA data"])
+router.include_router(malaysia.usa_router, prefix='/malaysia', tags=["Malaysia data"])
