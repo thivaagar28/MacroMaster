9a4fbc686bb7460cf939389f3df400bb552dada1
thivaagar28 <thivaagar28@gmail.com>
2024-05-24 03:33:15 +0800

    Added functions to retrieve features, composite index and added response model for each one of them

--- backend/app/services/usa_service.py
+++ backend/app/services/usa_service.py
@@ -4,25 +4,21 @@ from datetime import datetime, timedelta
 from bson import datetime as bson_datetime
 #import database models, schemes
 from app.models.usa_model import USA
-from app.schemas.usa_schema import StatPoc1, StatPoc6, StatPoc12, Month
+from app.schemas.usa_schema import StatPoc1, StatPoc12, Month, FeaturesPoc1, FeaturesPoc12, CmptPoc1, CmptPoc12
 
 start_date = datetime.strptime('1992-01', '%Y-%m')
 cutoff_date = datetime.strptime('2024-05', '%Y-%m')
+feature_start_date = datetime.strptime('2020-05', '%Y-%m')
 
 class USAService:
     @staticmethod
     async def get_months_list_poc1() -> List[Month]:
-        return await USA.find(USA.month >= start_date, USA.month <= cutoff_date).sort().to_list()
-    
-    @staticmethod
-    async def get_months_list_poc6() -> List[Month]:
-        temp_start_date = start_date + timedelta(31 *5) # add 6 months
-        return await USA.find(USA.month >= temp_start_date, USA.month <= cutoff_date).sort().to_list()
+        return await USA.find(USA.month >= start_date, USA.month <= cutoff_date).sort(-USA.month).to_list()
     
     @staticmethod
     async def get_months_list_poc12() -> List[Month]:
         temp_start_date = start_date + timedelta(31 *11) # add 12 months
-        return await USA.find(USA.month >= temp_start_date, USA.month <= cutoff_date).sort().to_list()
+        return await USA.find(USA.month >= temp_start_date, USA.month <= cutoff_date).sort(-USA.month).to_list()
     
     @staticmethod
     async def get_stat_poc1(month: datetime) -> StatPoc1:
@@ -31,14 +27,6 @@ class USAService:
             raise pymongo.errors.OperationFailure(f"POC 1 stat data for {month} not found")
         
         return stat_poc1
-
-    @staticmethod
-    async def get_stat_poc6(month: datetime) -> StatPoc6:
-        stat_poc6 = await USA.find_one(USA.month == month)
-        if not stat_poc6:
-            raise pymongo.errors.OperationFailure(f"POC 6 stat data for {month} not found")
-        
-        return stat_poc6
     
     @staticmethod
     async def get_stat_poc12(month: datetime) -> StatPoc12:
@@ -47,3 +35,21 @@ class USAService:
             raise pymongo.errors.OperationFailure(f"POC 12 stat data for {month} not found")
         
         return stat_poc12
+    
+    @staticmethod
+    async def get_poc1_features() -> FeaturesPoc1:
+        return await USA.find(USA.month >= feature_start_date, USA.month <= cutoff_date).sort(USA.month).to_list()
+    
+    @staticmethod
+    async def get_poc12_features() -> FeaturesPoc12:
+        return await USA.find(USA.month >= feature_start_date, USA.month <= cutoff_date).sort(USA.month).to_list()
+    
+    @staticmethod
+    async def get_poc1_cmpt() -> CmptPoc1:
+        temp_cutoff_date = cutoff_date + timedelta(31) # add 1 months
+        return await USA.find(USA.month >= feature_start_date, USA.month <= temp_cutoff_date).sort(USA.month).to_list()
+    
+    @staticmethod
+    async def get_poc12_cmpt() -> CmptPoc12:
+        temp_cutoff_date = cutoff_date + timedelta(31 * 12) # add 12 months
+        return await USA.find(USA.month >= feature_start_date, USA.month <= temp_cutoff_date).sort(USA.month).to_list()
