5ab7160342915ece6f30bef08347ed37ec85c196
thivaagar28 <thivaagar28@gmail.com>
2024-05-25 18:08:04 +0800

    Added positive and negative correlation trend check functions, added according to the macro correlations

--- frontend/src/components/Home/MacroIndex.jsx
+++ frontend/src/components/Home/MacroIndex.jsx
@@ -5,10 +5,14 @@ import { MacroStat } from './MacroStat'
 
 export const MacroIndex = ({data, country}) => {
   
-  const trend_check = (value) => {
+  const trend_check_p = (value) => {
     return (value > 0)
   }
-  
+    
+  const trend_check_n = (value) => {
+    return (value < 0)
+  }
+
   return (
     <Flex mt={3} bg="RGBA(100,84,132, 0.51)" h={'40%'} borderRadius="2xl" position={'relative'} flexDir={'row'} justifyContent={'flex-start'}>
         <Flex bg={'#47406F'} w={'5%'} h={"full"}  borderTopLeftRadius={'2xl'} borderBottomLeftRadius={'2xl'} justifyContent={'center'} alignItems={'center'}>
@@ -31,17 +35,17 @@ export const MacroIndex = ({data, country}) => {
             <Divider my={1}/>
             <Flex flex={1} px={5}>
               <SimpleGrid w={'full'} columns={4} spacingX='5' spacingY='5px'>
-                <MacroStat name='Consumer Price Index (CPI)' perc={data.cpi} trend={trend_check(data.cpi)} />
-                <MacroStat name='Inflation Growth Rate' perc={data.inf} trend={trend_check(data.inf)} /> 
-                <MacroStat name='Goverment Reserves (Billions)' perc={data.ipi} trend={trend_check(data.cpi)} />
-                <MacroStat name='Industrial Production Index' perc={data.ipi} trend={trend_check(data.ipi)} />
-                <MacroStat name='Long Term Interest Rate' perc={data.lt_ir} trend={trend_check(data.lt_ir)} />
-                <MacroStat name='Short Term Interest Rate' perc={data.st_ir} trend={trend_check(data.st_ir)} />
-                <MacroStat name='Gross Domestic Product (GDP)' perc={data.gdpt} trend={trend_check(data.gdpt)} />
-                <MacroStat name='Unemployment Rate'  perc={data.ur} trend={trend_check(data.ur)}/>
-                <MacroStat name='Producer Price Index (PPI)'  perc={data.ppi} trend={trend_check(data.ppi)}/>
-                <MacroStat name='Wholesale & Retail Trade'  perc={data.wrt} trend={trend_check(data.wrt)}/>
-                <MacroStat name='Business Cycle Indicators (BCI)' perc={data.bcli} trend={trend_check(data.bcli)}/>
+                <MacroStat name='Consumer Price Index (CPI)' perc={data.cpi} trend={trend_check_p(data.cpi)} />
+                <MacroStat name='Inflation Growth Rate' perc={data.inf} trend={trend_check_n(data.inf)} /> 
+                <MacroStat name='Goverment Reserves (Billions)' perc={data.ipi} trend={trend_check_p(data.cpi)} />
+                <MacroStat name='Industrial Production Index' perc={data.ipi} trend={trend_check_p(data.ipi)} />
+                <MacroStat name='Long Term Interest Rate' perc={data.lt_ir} trend={trend_check_n(data.lt_ir)} />
+                <MacroStat name='Short Term Interest Rate' perc={data.st_ir} trend={trend_check_n(data.st_ir)} />
+                <MacroStat name='Gross Domestic Product (GDP)' perc={data.gdpt} trend={trend_check_p(data.gdpt)} />
+                <MacroStat name='Unemployment Rate'  perc={data.ur} trend={trend_check_n(data.ur)}/>
+                <MacroStat name='Producer Price Index (PPI)'  perc={data.ppi} trend={trend_check_n(data.ppi)}/>
+                <MacroStat name='Wholesale & Retail Trade'  perc={data.wrt} trend={trend_check_p(data.wrt)}/>
+                <MacroStat name='Business Cycle Indicators (BCI)' perc={data.bcli} trend={trend_check_p(data.bcli)}/>
                 <Flex justifyContent={'center'} alignItems={'center'}>Data cut off Date: </Flex>
               </SimpleGrid>
             </Flex>
