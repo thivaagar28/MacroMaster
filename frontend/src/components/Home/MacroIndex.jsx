8868e0f9ace459bfd588c306cbaae8f44673f1af
thivaagar28 <thivaagar28@gmail.com>
2024-05-27 18:21:17 +0800

    Added registered user clickable macro data

--- frontend/src/components/Home/MacroIndex.jsx
+++ frontend/src/components/Home/MacroIndex.jsx
@@ -2,9 +2,12 @@ import { Divider, Flex, Text, Spinner } from '@chakra-ui/react'
 import { IndexStat } from './IndexStat'
 import { SimpleGrid } from '@chakra-ui/react'
 import { MacroStat } from './MacroStat'
+import { MacroStatLink} from './MacroStatLink'
+import { useAuth } from '../../hooks/useAuth';
 
 export const MacroIndex = ({data, country}) => {
-  
+  const auth = useAuth();
+
   const trend_check_p = (value) => {
     return (value > 0)
   }
@@ -35,6 +38,8 @@ export const MacroIndex = ({data, country}) => {
             <Divider my={1}/>
             <Flex flex={1} px={5}>
               <SimpleGrid w={'full'} columns={4} spacingX='5' spacingY='5px'>
+                {!auth?.isAuthenticated ? 
+                <>
                 <MacroStat name='Consumer Price Index (CPI)' perc={data.cpi} trend={trend_check_p(data.cpi)} />
                 <MacroStat name='Inflation Growth Rate' perc={data.inf} trend={trend_check_n(data.inf)} /> 
                 <MacroStat name='Goverment Reserves (Billions)' perc={data.ipi} trend={trend_check_p(data.cpi)} />
@@ -46,7 +51,22 @@ export const MacroIndex = ({data, country}) => {
                 <MacroStat name='Producer Price Index (PPI)'  perc={data.ppi} trend={trend_check_n(data.ppi)}/>
                 <MacroStat name='Wholesale & Retail Trade'  perc={data.wrt} trend={trend_check_p(data.wrt)}/>
                 <MacroStat name='Business Cycle Indicators (BCI)' perc={data.bcli} trend={trend_check_p(data.bcli)}/>
-                <Flex justifyContent={'center'} alignItems={'center'}>Data cut off Date: </Flex>
+                </> : 
+                <>
+                <MacroStatLink name='Consumer Price Index (CPI)' perc={data.cpi} trend={trend_check_p(data.cpi)} country={country} index={'cpi'}/>
+                <MacroStatLink name='Inflation Growth Rate' perc={data.inf} trend={trend_check_n(data.inf)} country={country} index={'inf'}/> 
+                <MacroStatLink name='Goverment Reserves (Billions)' perc={data.ipi} trend={trend_check_p(data.cpi)} country={country} index={'gr'}/>
+                <MacroStatLink name='Industrial Production Index' perc={data.ipi} trend={trend_check_p(data.ipi)} country={country} index={'ipi'}/>
+                <MacroStatLink name='Long Term Interest Rate' perc={data.lt_ir} trend={trend_check_n(data.lt_ir)} country={country} index={'lt_ir'}/>
+                <MacroStatLink name='Short Term Interest Rate' perc={data.st_ir} trend={trend_check_n(data.st_ir)} country={country} index={'st_ir'}/>
+                <MacroStatLink name='Gross Domestic Product (GDP)' perc={data.gdpt} trend={trend_check_p(data.gdpt)} country={country} index={'gdp'}/>
+                <MacroStatLink name='Unemployment Rate'  perc={data.ur} trend={trend_check_n(data.ur)} country={country} index={'ur'}/>
+                <MacroStatLink name='Producer Price Index (PPI)'  perc={data.ppi} trend={trend_check_n(data.ppi)} country={country} index={'ppi'}/>
+                <MacroStatLink name='Wholesale & Retail Trade'  perc={data.wrt} trend={trend_check_p(data.wrt)} country={country} index={'wrt'}/>
+                <MacroStatLink name='Business Cycle Indicators (BCI)' perc={data.bcli} trend={trend_check_p(data.bcli)} country={country} index={'bcli'}/>
+                </>
+                }
+                <Flex border={'3px solid'} borderColor={'#d4a600'} justifyContent={'center'} alignItems={'center'}>Data Cut-Off Date: 17th May 24</Flex>
               </SimpleGrid>
             </Flex>
           </Flex> : 
