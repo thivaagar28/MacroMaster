4f648b1a4d206eaa5e262046a4ab52798e974600
thivaagar28 <thivaagar28@gmail.com>
2024-05-28 10:51:08 +0800

    matched color coding to gauge

--- frontend/src/components/Home/NumberDisplay.jsx
+++ frontend/src/components/Home/NumberDisplay.jsx
@@ -3,14 +3,12 @@ import { Box, Text } from '@chakra-ui/react';
 
 export const NumberDisplay = ({number}) => {
     let bgColor;
-    if (number > 0 && number < 15) {
-        bgColor = 'red.500';
-    } else if (number >= 15 && number < 35) {
-        bgColor = 'yellow.500';
-    } else if (number >= 35 && number < 65) {
+    if (number > 0 && number < 33) {
         bgColor = 'green.500';
-    } else if (number >= 65 && number < 85) {
+    } else if (number >= 33 && number < 65) {
         bgColor = 'yellow.500';
+    } else if (number >= 65 && number < 85) {
+        bgColor = 'orange.500';
     } else if (number >= 85 && number <= 100) {
         bgColor = 'red.500';
     } else {
